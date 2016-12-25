7fe7cd14500a5e458407e8750d40b9cae59b82b4
faraxnak <farid@farids-macbook-pro.local>
2016-12-25 13:35:16 +0330

    Initial Commit

---
+++ TtroGooeyTabbar/TtroGooeyTabbar.h
@@ -0,0 +1,19 @@
+//
+//  TtroGooeyTabbar.h
+//  TtroGooeyTabbar
+//
+//  Created by Farid on 12/25/16.
+//  Copyright © 2016 ParsPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TtroGooeyTabbar.
+FOUNDATION_EXPORT double TtroGooeyTabbarVersionNumber;
+
+//! Project version string for TtroGooeyTabbar.
+FOUNDATION_EXPORT const unsigned char TtroGooeyTabbarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TtroGooeyTabbar/PublicHeader.h>
+
+
